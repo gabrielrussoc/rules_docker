7982025855ebc332032cb9c9593ad20b231378c1
partha-db <parthasarathy.saravanan@databricks.com>
2024-10-08 14:56:23 +0000

    Vendor tarfile from py3.6 to ensure consistent tarfile generation across all py3 versions

--- container/force_py3.py
+++ container/force_py3.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     argv = sys.argv
     path, _ = os.path.split(argv[0])
     argv[0] = os.path.join(path, 'build_tar.py')
-    argv = ['/usr/bin/env', 'python2'] + argv
+    argv = ['/usr/bin/env', 'python3'] + argv
     os.execv(argv[0], argv)
